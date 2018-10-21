6e60ffb252f6bfdebc426c7de67af5574fec7d53
Guillaume Limberger <guillaume.limberger@lecnam.net>
2018-10-21 11:35:57 +0200

    Initial page.

--- symfony/assets/components/DisplayState.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default (props) => {
-  return (
-    <div className="alert alert-primary small" role="alert">
-      <pre className='mb-0'>{JSON.stringify(props.state, null, 2)}</pre>
-    </div>
-  )
-}
--- symfony/assets/js/foo.js
+++
@@ -1,9 +0,0 @@
-export default class Foo {
-  constructor (foo) {
-    this._foo = foo
-  }
-
-  click() {
-    return window.alert(this._foo)
-  }
-}
